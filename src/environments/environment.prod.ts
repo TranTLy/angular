cfba58ee52010886fb0a16ddb6957f6385bee89b
TranLy <1612367@student.hcmus.edu.vn>
2019-09-06 09:18:27 +0700

    initial project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCkNQHYtyK69GOWvYDSkj4O7mGtRyO3n5s",
+    authDomain: "angulardemo-f43d3.firebaseapp.com",
+    databaseURL: "https://angulardemo-f43d3.firebaseio.com",
+    projectId: "angulardemo-f43d3",
+    storageBucket: "",
+    messagingSenderId: "175106224140",
+    appId: "1:175106224140:web:7ffb7b8aae4ee9a5707e29"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCkNQHYtyK69GOWvYDSkj4O7mGtRyO3n5s",
+    authDomain: "angulardemo-f43d3.firebaseapp.com",
+    databaseURL: "https://angulardemo-f43d3.firebaseio.com",
+    projectId: "angulardemo-f43d3",
+    storageBucket: "",
+    messagingSenderId: "175106224140",
+    appId: "1:175106224140:web:7ffb7b8aae4ee9a5707e29"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
